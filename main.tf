7fa01d9dd1fd2630b3a26253b1b4a45e50c829c6
bhaskarrao1 <baipothubhaskar@gmail.com>
2023-09-10 11:19:01 +0530

    .tf files

---
+++ main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key    = "AKIA4ZRWCPBCEYNO4B6T"
+  secret_key    = "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"
+}
+resource "aws_vpc" "my-vpc" {
+    name = myvpc
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support = true
+}
+resource "aws_subnet" "subnet" {
+  name = my-subnent
+  vpc_id = aws_vpc.my-vpc
+  availability_zone = "us-east-2a"
+  cidr_block = [10.0.0.0/24]
+}
+resource "aws_security_group" "SG" {
+    name = my-sg
+    depends_on = [ aws_subnet.subnet , aws_vpc.my-vpc ]
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = tcp
+        cidr_blocks = [0.0.0.0/0]
+    }
+    egress {
+        from_port = 8080
+        to_port = 8080
+        cidr_blocks = [0.0.0.0/0] 
+        protocol = tcp 
+    }
+}    
---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+    type = string
+    default = "my-accesskey"
+}
+variable "secret_key" {
+    type = string
+    default = "my-secretkey"
+     
+}
